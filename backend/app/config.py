99a0df83f06a37f69656ebb38c3efbd3504bf3e4
Randysman <134296647+Randysman@users.noreply.github.com>
2026-01-13 15:29:36 +0300

    create configs

---
+++ backend/app/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = "Shop FastAPI"
+    debug: bool = True
+    database_url : str = "sqlite:/// ./shop.db"
+    cors_origins : list = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ]
+    static_dir: str = "static"
+    images_dir: str = "static/images"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
