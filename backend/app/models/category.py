3fe86a9d2bb67754edebab361439892b43ded852
Randysman <134296647+Randysman@users.noreply.github.com>
2026-01-14 17:12:21 +0300

    add models for category and products

---
+++ backend/app/models/category.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy import Column, Integer, String
+from ..database import Base
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, unique=True, nullable=False, index=True)
+    slug: Mapped[str] = mapped_column(String, unique=True, nullable=False, index=True)
+
+    products = relationship("Product", back_populates="category")
+
+    def __repr__(self):
+        return f"Category: id={self.id}, name={self.name}"
---
+++ backend/app/models/product.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy import Column, Integer, String, Text, Float, DateTime, ForeignKey
+from ..database import Base
+from datetime import datetime
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, unique=True, nullable=False, index=True)
+    description: Mapped[str] = mapped_column(Text)
+    price: Mapped[float] = mapped_column(Float, nullable=False)
+    category_id: Mapped[int] = mapped_column(Integer, ForeignKey("categories.id"), nullable=False)
+    image_url: Mapped[str] = mapped_column(String)
+    created_at: Mapped[str] = mapped_column(DateTime, default=datetime.utcnow)
+
+    category = relationship("Category", back_populates="products")
+
+    def __repr__(self):
+        return f"Category: id={self.id}, name={self.name}, price={self.price}"
