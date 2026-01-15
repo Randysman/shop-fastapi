558cd78594bf9d8c0b8989eef2a18cb423037b3f
Randysman <134296647+Randysman@users.noreply.github.com>
2026-01-15 17:13:21 +0300

    add schemas for category

---
+++ backend/app/schemeas/category.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+
+
+class CategoryBase(BaseModel):
+    name: str = Field(..., min_length=4, max_length=25, description="Category name")
+    slug: str = Field(..., min_length=4, max_length=25, description="Slug")
+
+
+class CategoryCreate(CategoryBase):
+    pass
+
+
+class CategoryResponse(CategoryBase):
+    id: int = Field(..., description="Category ID")
+
+    class Config:
+        form_attributes = True
